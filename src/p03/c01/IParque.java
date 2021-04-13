b6f73a1a9b687f082ff8ef002d7ec217dd3a5501
asl1001 <asl1001@alu.ubu.es>
2021-04-13 09:38:36 +0200

    Declaracion en la interfaz del metodo salirDelParque

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -3,6 +3,8 @@ package src.p03.c01;
 public interface IParque {
 	
 	public abstract void entrarAlParque(String puerta);
+	//declaracion en la interfaz del metodo salir
+	public abstract void salirDelParque(String puerta);
 
 	// TODO
 
